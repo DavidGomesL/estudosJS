1ebce5c3189083810d8237e186b12cff3382ea2b
DavLuan <davidgomes1194@gmail.com>
2024-03-11 11:21:32 -0300

    primeiro commit

---
+++ arreysEobj/script.js
@@ -0,0 +1,13 @@
+//let abacaxi = [1,2,3,'olá mundo',['morango','uva']]
+
+let obj = [{
+    idade: 29,
+    nome: 'David',
+    ano: 2024,
+},'uva','maçã','salada mista']
+
+let soma = obj.idade + obj.ano;
+
+//console.log(obj[0].idade);
+
+console.log("Meu nome é"+" "+obj[0].nome+" "+"e tenho "+obj[0].idade+" "+"anos.")
---
+++ ifAndElse.js
@@ -0,0 +1,3 @@
+//--------------Exercicio If/Else -----------
+//1. Escreva	 um	 programa	 para	 ler	 2	 valores	 (considere	 que	 	 não	 serão	
+//informados	valores	iguais)	e	escrever	o	maior	deles.
