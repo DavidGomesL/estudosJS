b312cc3d9d58d7c9027301aef75d59d157406f70
DavLuan <davidgomes1194@gmail.com>
2024-03-11 11:45:13 -0300

    exerc. de 1 a 3

--- ifAndElse.js
+++ ifAndElse.js
@@ -1,3 +1,51 @@
-//--------------Exercicio If/Else -----------
+//--------------IFCE - Exercicio If/Else -----------
 //1. Escreva	 um	 programa	 para	 ler	 2	 valores	 (considere	 que	 	 não	 serão	
 //informados	valores	iguais)	e	escrever	o	maior	deles.
+
+//let num1 = 4;
+//let num2 = 3;
+//
+//if (num1 > num2) {
+//    console.log(num1);
+//} else if (num1 < num2) {
+//    console.log(num2);
+//}
+//-------------------------------------------------
+
+//2. Escreva	 um	 programa	 para	 ler	 o	 ano	 de	 nascimento	 de	 uma	 pessoa	 e	
+//escrever	 uma	 mensagem	 que	 diga	 se	 ela	 poderá	 ou	 não	 votar	 este	 ano	
+//(não	é	necessário	considerar	o	mês	em	que	ela	nasceu).
+
+//let anoNasc = 2009;
+//let anoAtual =2024;
+//let podeVotar = anoAtual - anoNasc;
+
+//if (podeVotar >= 16) {
+//    console.log(`Poderá votar este ano.`)
+//} else if (podeVotar < 16) {
+//    console.log(`Não poderá votar este ano.`)
+//}
+//-------------------------------------------------
+
+//3. Escreva	 um	 programa	 que	 verifique	 a	 validade	 de	 uma	 senha	 fornecida	
+//pelo	 usuário.	 A	 senha	 válida	 é	 o	 número	 1234.	Devem	 ser	impressas	 as	
+//seguintes	mensagens:	
+//ACESSO	PERMITIDO	caso	a	senha	seja	válida.	
+//ACESSO	NEGADO	caso	a	senha	seja	inválida.
+
+//let senha = 1244;
+
+//if (senha === 1234) {
+//    console.log(`ACESSO PERMITIDO.`)
+//} else if (senha !== 1234) {
+//    console.log(`ACESSO NEGADO.`)
+//}
+//--------------------------------------------------
+
+
+
+
+
+
+
+
