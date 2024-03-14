fe81251ac445f1d576247f36bd75ae380b944080
DavLuan <davidgomes1194@gmail.com>
2024-03-14 19:01:29 -0300

    continuidade dos exercicios

--- ifAndElse.js
+++ ifAndElse.js
@@ -208,7 +208,50 @@
 //    o valor do aumento;
 //    o novo salário, após o aumento.
 
+//let salario = 2500;
+//
+//if (salario <= 280) {
+//
+//    let percentual = 1.2 - 1;
+//    let valorAumento = salario * 0.2;
+//    let novoSalario = salario + valorAumento;
+//
+//    console.log(`O salário atual é R$ ${salario}, o percentual de //aumento foi de ${percentual.toFixed(2)}%, o valor do aumento foi //de R$ ${valorAumento.toFixed(2)} e o novo salário é R$ $//{novoSalario.toFixed(2)}.`)
+//} else if (salario > 280 && salario <= 700) {
+//
+//    let percentual = 1.15 - 1;
+//    let valorAumento = salario * 0.15;
+//    let novoSalario = salario + valorAumento;
+//
+//    console.log(`O salário atual é R$ ${salario}, o percentual de //aumento foi de ${percentual.toFixed(2)}%, o valor do aumento foi //de R$ ${valorAumento.toFixed(2)} e o novo salário é R$ $//{novoSalario.toFixed(2)}.`)
 
+//}//else if (salario > 700 && salario <= 1500) {
+
+//    let percentual = 1.10 - 1;
+//    let valorAumento = salario * 0.10;
+ //   let novoSalario = salario + valorAumento;
+//
+//    console.log(`O salário atual é R$ ${salario}, o percentual de //aumento foi de ${percentual.toFixed(2)}%, o valor do aumento foi //de R$ ${valorAumento.toFixed(2)} e o novo salário é R$ $//{novoSalario.toFixed(2)}.`)
+//
+//} else if (salario > 1500) {
+//
+//    let percentual = 1.05 - 1;
+//    let valorAumento = salario * 0.05;
+//    let novoSalario = salario + valorAumento;
+//
+//    console.log(`O salário atual é R$ ${salario}, o percentual de //aumento foi de ${percentual.toFixed(2)}%, o valor do aumento foi //de R$ ${valorAumento.toFixed(2)} e o novo salário é R$ $//{novoSalario.toFixed(2)}.`)
+
+//}
+//-----------------------------------------------------------------
+//2)Faça um script que lê as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre, e calcule a sua média. A atribuição de conceitos obedece à tabela abaixo:
+//
+//      Média de Aproveitamento  Conceito
+//      Entre 9.0 e 10.0                      A
+//      Entre 7.5 e 9.0                        B
+//      Entre 6.0 e 7.5                        C
+ //     Entre 4.0 e 6.0                        D
+//      Entre 4.0 e zero                      E
+//    O algoritmo deve mostrar numa janela de alertas notas, a média, o conceito correspondente e a mensagem “APROVADO” se o conceito for A, B ou C ou “REPROVADO” se o conceito for D ou E.
 
 
 
