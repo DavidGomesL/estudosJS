75e4fd5bec2a05e2c1573c27229aa39975a08ba6
YoInsider <insensoscoffee17@proton.me>
2024-03-21 14:56:44 -0300

    adicao de exercicios com for

---
+++ for/script.js
@@ -0,0 +1,46 @@
+// Iterando sobre um array e verificando se os números são pares ou ímpares:
+//let nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+//let pares = [];
+//let impares = [];
+//
+//for(let i = 0; i < nums.length; i++) {
+//    if(nums[i] % 2 === 0) {
+//        pares.push(nums[i])
+//    } else {
+//        impares.push(nums[i])
+//    }
+//}
+//console.log(pares)
+//console.log(impares)
+//--------------------------------------------------------------------------------
+//Encontrando a soma dos números pares em um intervalo:
+//let nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+//let soma = 0;
+//
+//for(let i = 0; i < nums.length; i++) {
+//    
+//    if(nums[i] % 2 === 0) {
+//        //soma += nums[i] ///Está adcionando o número par a soma
+//        console.log(soma += nums[i]) //Está mostrando a lista com o valor de soma dos número pares
+//    }
+//}
+
+//console.log(soma) // Está imprimindo no console, o valor da soma total
+//--------------------------------------------------------------------------
+//Percorrer um array de objetos e mostrar somente os que tem preço maior que 30: 
+//
+//const produtos = [
+//    { nome: "Camiseta", preco: 25.00 },
+//    { nome: "Calça", preco: 35.00 },
+//    { nome: "Boné", preco: 15.00 },
+//    { nome: "Jaqueta", preco: 50.00 }
+//];
+//
+//for(let i = 0; i < produtos.length; i++) {
+//    if(produtos[i].preco > 30) { //Está verificando quais produtos possuem valor maior que 30
+//        console.log(produtos[i]) //Está imprimindo os produtos que possuem valor maior que 30
+//    }
+//}
+//----------------------------------------------------------------------------------
+
+
