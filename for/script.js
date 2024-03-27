17b53a7257c1f9b9cb704c8430be57eef285fb87
DavidGomesL <>
2024-03-27 18:26:58 -0300

    Adicionada novas questões

--- for/script.js
+++ for/script.js
@@ -155,7 +155,7 @@
 //Verificar se na frase "PHP morreu" tem alguma letra e.
 //let frase = "PHP morreu";
 //let arrayFrase = frase.split(''); //Transforma a string da variável "frase" em array: ["P","H","P",...]
-//let encontrouE = false; //Essa vari´´avel será usada para indicar se a letra "e" foi encontrada na string
+//let encontrouE = false; //Essa variavel será usada para indicar se a letra "e" foi encontrada na string
 //
 //for(let i = 0; i < arrayFrase.length; i++){
 //    //Verifica se o elemento atual é estritamente igual a letra "e"
@@ -170,6 +170,82 @@
 //} else {
 //    console.log(`Não possui a letra "e".`) //Se não encontrou a letra "e", imprime a mensagem "Não possui a letra 'e'"
 //}
+//------------------------------------------------------------------------------
+//let aluno = [
+//    {
+//        id:1,
+//        nome:"Josefina",
+//        matricula :435243,
+//        notaFinal:6
+//    },
+//    {
+//        id:2,
+//        nome:"Carlos",
+//        matricula :455243,
+//        notaFinal:3
+//    },
+//    {
+//        id:3,
+//        nome:"Joao",
+//        matricula :54313,
+//        notaFinal:2
+//    },
+//    {
+//        id:4,
+//        nome:"Carlota",
+//        matricula :9124810,
+//        notaFinal:10
+//    },
+//    {
+//        id:5,
+//        nome:"Giusseppe",
+//        matricula :75635,
+//        notaFinal:8
+//    },
+//    {
+//        id:6,
+//        nome:"Joaquina",
+//        matricula :231423,
+//        notaFinal:4
+//    },
+//    {
+//        id:7,
+//        nome:"Marquim",
+//        matricula :35253,
+//        notaFinal:9
+//    },
+//]
+//
+//let alunosAprovados = [];
+//let alunosReprovados = [];
+//
+////Quero quem me retornem os alunos que passaram por média, tomem consideração que 7 é o valor para se passar por media. Quero que me retornem a seguinte frase
+//
+////O aluno ... de matricula ... passou com nota ...
+////Se nao 
+////O aluno ... de matricula ... não passou, ligue para a diretora
+//
+//for(let i = 0; i < aluno.length; i++) {
+//    if(aluno[i].notaFinal >= 7) {
+//        alunosAprovados.push(`O aluno ${aluno[i].nome} de matricula ${aluno[i].matricula} passou com nota ${aluno[i].notaFinal}.`)
+//    } else {
+//        alunosReprovados.push(`O aluno ${aluno[i].nome} de matricula ${aluno[i].matricula} não passou, ligue para a diretora.`)
+//    }
+//};
+//console.log(alunosAprovados,alunosReprovados);
+//---------------------------------------------------------------------------------
+//Quero que vocês calculem quantos anos bissextos existem entre 1900 - 2024
+
+//let contadorAno = 0;
+//
+//for(let i = 1900; i <= 2024; i++){
+//    if(i % 4 === 0){
+//        contadorAno++
+//    }
+//};
+//console.log(contadorAno);
+//----------------------------------------------------------------------------------
+
 
 
 
